9f12af97df14ae94f4b20c3b0b87d4709d42fa74
Guohao Ouyang <xou96@my.yorku.ca>
2021-03-25 19:44:11 -0400

    Updates

---
+++ precache-manifest.974748ed40a6391a5c897bd774d06ed1.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d9cc9aad3139ba9e83f5cf37bbd04d",
+    "url": "/spa-brochure-inovagame/index.html"
+  },
+  {
+    "revision": "2e3e081401cd206117ed",
+    "url": "/spa-brochure-inovagame/static/css/main.a7d0ca3d.chunk.css"
+  },
+  {
+    "revision": "fd52babff37f959fc996",
+    "url": "/spa-brochure-inovagame/static/js/2.292bda53.chunk.js"
+  },
+  {
+    "revision": "eae48a3a6584dfcac73d0b0f2afbf15a",
+    "url": "/spa-brochure-inovagame/static/js/2.292bda53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3e081401cd206117ed",
+    "url": "/spa-brochure-inovagame/static/js/main.e70ef6f0.chunk.js"
+  },
+  {
+    "revision": "980dcfaddfa9405d11fa",
+    "url": "/spa-brochure-inovagame/static/js/runtime-main.25631a3a.js"
+  },
+  {
+    "revision": "7e2398c6a4d450bd140ed2ee7895db84",
+    "url": "/spa-brochure-inovagame/static/media/CoralGear.7e2398c6.png"
+  },
+  {
+    "revision": "c3417738214e55637ee2c9ab49eb6628",
+    "url": "/spa-brochure-inovagame/static/media/Ekko_left.c3417738.jpeg"
+  },
+  {
+    "revision": "9a2aa9ee71a51e3846c544cc43d362b0",
+    "url": "/spa-brochure-inovagame/static/media/Jhin.9a2aa9ee.png"
+  },
+  {
+    "revision": "fb228e04c44303be6ad84e12238976d5",
+    "url": "/spa-brochure-inovagame/static/media/JinxSquare.fb228e04.png"
+  },
+  {
+    "revision": "0b766c1f030172303240fb92d5278799",
+    "url": "/spa-brochure-inovagame/static/media/KaisaD.0b766c1f.png"
+  },
+  {
+    "revision": "1170b63fc38deb42469cd2d4c6f4ff69",
+    "url": "/spa-brochure-inovagame/static/media/LilliaSquare.1170b63f.png"
+  },
+  {
+    "revision": "5a1026c35a3fba4f94691ca60ac4167b",
+    "url": "/spa-brochure-inovagame/static/media/MustardGear.5a1026c3.png"
+  },
+  {
+    "revision": "21563b1ae07ba93d217fc6c4189789c8",
+    "url": "/spa-brochure-inovagame/static/media/Sheen.21563b1a.png"
+  },
+  {
+    "revision": "f71f2b20ad8b043358b3e4806254b729",
+    "url": "/spa-brochure-inovagame/static/media/SylasSquare.f71f2b20.png"
+  },
+  {
+    "revision": "7cc7cc844880c1203a8d9473d2d62bb1",
+    "url": "/spa-brochure-inovagame/static/media/TealGear.7cc7cc84.png"
+  },
+  {
+    "revision": "77ae637f8b831f630ce2d25c9fa35209",
+    "url": "/spa-brochure-inovagame/static/media/XayahSquare.77ae637f.png"
+  },
+  {
+    "revision": "ea44d96d8eec2cad8c38d73c56076890",
+    "url": "/spa-brochure-inovagame/static/media/castledown-heavy.woff.ea44d96d.ttf"
+  },
+  {
+    "revision": "422b4e98ad42c083d5683aaabb4e1737",
+    "url": "/spa-brochure-inovagame/static/media/circle-gradient.422b4e98.svg"
+  },
+  {
+    "revision": "01d29bfc497a0e5560b15f08b4a51bd7",
+    "url": "/spa-brochure-inovagame/static/media/ekko.01d29bfc.jpeg"
+  },
+  {
+    "revision": "d5dc0ed53dbda4fe8121bfaa51a15b64",
+    "url": "/spa-brochure-inovagame/static/media/ekkobot.d5dc0ed5.jpeg"
+  },
+  {
+    "revision": "02c48e016f98c4fac55915fec2f9bdac",
+    "url": "/spa-brochure-inovagame/static/media/jhinbot.02c48e01.png"
+  },
+  {
+    "revision": "633324c84146a4f9a0f49fa80b8f1df1",
+    "url": "/spa-brochure-inovagame/static/media/jihntop.633324c8.png"
+  },
+  {
+    "revision": "3bea8779cf296153dcfecbe51706fe83",
+    "url": "/spa-brochure-inovagame/static/media/kaisaleft.3bea8779.png"
+  },
+  {
+    "revision": "2f4f5dd8329981583206818c325b473f",
+    "url": "/spa-brochure-inovagame/static/media/kaisatop.2f4f5dd8.jpeg"
+  },
+  {
+    "revision": "8a3f8f56f1c0b24897be0f8fa23d918d",
+    "url": "/spa-brochure-inovagame/static/media/leftCloud.8a3f8f56.png"
+  },
+  {
+    "revision": "ff3c8c5655c6aee16987b3b8f0542599",
+    "url": "/spa-brochure-inovagame/static/media/rightCloud.ff3c8c56.png"
+  },
+  {
+    "revision": "db1447c2dadbcbb93a5d232c07ea63b1",
+    "url": "/spa-brochure-inovagame/static/media/solid-arrow.db1447c2.svg"
+  },
+  {
+    "revision": "7b161b9abb25dc4250b40cbdcbbcfba4",
+    "url": "/spa-brochure-inovagame/static/media/watercolorr.7b161b9a.png"
+  }
+]);
